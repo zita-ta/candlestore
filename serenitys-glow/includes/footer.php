738e67fd80a1b63fd0e97fe593fa7ccefd2c0805
razita <razitasabrina2008@gmail.com>
2026-09-03 11:01:07 +0700

    Delete serenitys-glow directory

--- serenitys-glow/includes/footer.php
+++
@@ -1,6 +0,0 @@
-    <footer style="background-color:#fff; text-align:center; padding:24px; margin-top:40px; color:#999; font-size:14px; border-top:1px solid #eee;">
-        &copy; <?= date('Y') ?> Serenity's Glow. Semua hak cipta dilindungi.
-    </footer>
-
-</body>
-</html>
